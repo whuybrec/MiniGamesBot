8d6cd49f27dfe30331ffb8f14b378069a845685f
Wouter Huybrechts <wouter.huybrechts@student.kuleuven.ne>
2020-10-17 12:01:06 +0200

    updated private.py

--- Other/private.py
+++ Other/private.py
@@ -1,5 +1,6 @@
 class Private:
     TOKEN = "YOUR DISCORD BOT TOKEN HERE"
     DEV_ID = 0 # your discord ID here
+    DEV_IDS = {0: "devAlpha"} # replace 0 with your discord ID
     BOT_ID = [1] # your discord bot ID here (if you have a testbot, you can put that ID in here too)
     prefixes = dict()
